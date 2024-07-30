ebfd8e179feacddea81ba3eedc22baf4d2ab52a4
Jesús Escolar González <jesusescolargonzalez@gmail.com>
2024-07-30 18:43:37 +0200

    Add 3 msvcs and added to parent pom.xml

---
+++ msvc-config/src/main/java/com/msvc/config/MsvcConfigApplication.java
@@ -0,0 +1,13 @@
+package com.msvc.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcConfigApplication.class, args);
+	}
+
+}
---
+++ msvc-eureka/src/main/java/com/msvc/eureka/MsvcEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.msvc.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcEurekaApplication.class, args);
+	}
+
+}
