4f2bbb467f1ab48445b7453aab88a26fa44ae6cb
Jesús Escolar González <jesusescolargonzalez@gmail.com>
2024-10-11 20:01:17 +0200

    Added Service to Students Connection to Postgres in msvc-course Fix All

---
+++ msvc-course/src/test/java/com/msvc_course/MsvcCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msvc_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ msvc-student/src/test/java/com/msvc/student/MsvcStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msvc.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
